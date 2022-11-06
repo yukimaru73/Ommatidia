c334428f34f890ce062096aee2cc78354d97ab0f
yukimaru73 <ekxmekxm@gmail.com>
2022-11-06 17:25:48 +0900

    nya

--- Libs/Attitude.lua
+++ Libs/Attitude.lua
@@ -12,23 +12,23 @@ require("Libs.Quaternion")
 ---@field yawSpeed number
 Attitude = {
 
-	---@section new
 	---@param cls Attitude
+	---@param pitch number
+	---@param roll number
+	---@param yaw number
 	---@overload fun(cls:Attitude):Attitude creates a new zero-initialized Attitude
 	---@return Attitude
 	new = function(cls, pitch, roll, yaw)
-		local self = setmetatable({}, cls)
-		self.pitch = pitch or 0
-		self.roll = roll or 0
-		self.yaw = yaw or 0
-		self.rotation = Quaternion:_new()
-		self.pitchSpeed = 0
-		self.tiltLeftSpeed = 0
-		self.tiltUpSpeed = 0
-		self.compassSpeed = 0
-		return self
+		return LifeBoatAPI.lb_copy(cls, {
+			pitch = pitch or 0,
+			roll = roll or 0,
+			yaw = yaw or 0,
+			rotation = Quaternion:new(pitch or 0, roll or 0, yaw or 0),
+			pitchSpeed = 0,
+			rollSpeed = 0,
+			yawSpeed = 0
+		})
 	end;
-	---@endsection
 
 	---@section update
 	---@param self Attitude
@@ -80,4 +80,15 @@ Attitude = {
 		return self.rotation:getConjugateQuaternion():rotateVector(vector)
 	end;
 	---@endsection
+
+	---@section getFutureAttitude
+	---@param self Attitude
+	---@param time number
+	---@return Attitude
+	getFutureAttitude = function(self, time)
+		return Attitude:new(self.pitch + self.pitchSpeed * time, self.roll + self.rollSpeed * time, self.yaw + self.yawSpeed * time)
+	end;
+	---@endsection
+
+
 }
