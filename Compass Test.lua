08fad26db58a720ec085cdba5ce7b4ecbda6fccc
yukimaru73 <ekxmekxm@gmail.com>
2022-11-07 16:52:35 +0900

    maa

--- Compass Test.lua
+++ Compass Test.lua
@@ -1,7 +1,7 @@
 require("Libs.Attitude")
 
 A1 = Attitude:new()
-A1:update(0.125, 0, -0.25,-0.125)
-vec = { 1, 0, 0 }
+A1:update(0.125, 0.125, -0.25,0.25)
+vec = { 0, 0, 1 }
 vec2 = A1:rotateVectorLocalToWorld(vec)
 a = 0
