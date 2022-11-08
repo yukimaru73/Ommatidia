0c414f0ac88862f69a59b4de87f7816977f5ebc1
yukimaru73 <ekxmekxm@gmail.com>
2022-11-08 11:21:48 +0900

    nya

--- Libs/Attitude.lua
+++ Libs/Attitude.lua
@@ -37,6 +37,7 @@ Attitude = {
 	---@param tiltLeft number
 	---@param compass number
 	---@param tiltUp number
+	---@overload fun(self:Attitude, tiltFront:number, tiltLeft:number, compass:number):nil no tiltUp Mode
 	update = function(self, tiltFront, tiltLeft, compass, tiltUp)
 		
 		tiltUp = tiltUp or 0.25
--- test_a.lua
+++ test_a.lua
@@ -7,3 +7,7 @@ print((current-past+1.5)%1-0.5)
 print(current-past)--just get velocity
 print(((yaw + 1.75) % 1 - 0.5))--compass value to yaw(east~north~west~south~east)WTF?
 print((y-x+1.5)%1-0.5)
+compass = 0.01 * math.pi * 2
+yaw2 = -0.49 * math.pi * 2
+print(((compass - yaw2 + 3 * math.pi ) % (2*math.pi) - math.pi)/math.pi/2)
+print(nil or 3)
