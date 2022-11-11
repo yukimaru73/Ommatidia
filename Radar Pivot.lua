3fc147c68cab3fa955211261a0b2e7fb0bcd9fac
yukimaru73 <ekxmekxm@gmail.com>
2022-11-11 15:36:23 +0900

    nya-n

--- Radar Pivot.lua
+++ Radar Pivot.lua
@@ -9,7 +9,6 @@ ATTITUDE_BASE = Attitude:new(0, 0, 0)
 ATTITUDE_RADAR = Attitude:new(0, 0, 0)
 
 TARGET_POS = { 0, 0, 0 }
-TARGET_G_POS_P = { 0, 0, 0 }
 TARGET_G_POS_AVE = Average:new(5)
 TARGET_G_POS_AVE_P = Average:new(5)
 IS_TRACKING = false
@@ -24,23 +23,24 @@ function onTick()
 	TARGET_POS = { input.getNumber(1), input.getNumber(2), input.getNumber(3) }
 	ATTITUDE_BASE:update(input.getNumber(4), input.getNumber(5), input.getNumber(6), 0.25)
 	ATTITUDE_RADAR:update(input.getNumber(7), input.getNumber(8), input.getNumber(9), input.getNumber(10))
-	local gPos = { 0, 0, 0 }
+	local gPosRaw = { 0, 0, 0 }
+	local gVel = { 0, 0, 0 }
 	if input.getBool(1) and input.getBool(2) then --tracking on and target found
 		local lPos = { 0, 0, 0 }
-		gPos = ATTITUDE_RADAR:rotateVectorLocalToWorld(TARGET_POS)
+		gPosRaw = ATTITUDE_RADAR:rotateVectorLocalToWorld(TARGET_POS)
+		TARGET_G_POS_AVE:update(gPosRaw)
+		local averagedTargetPos = TARGET_G_POS_AVE:getAveragedTable()
+		debug.log("TST:->," ..averagedTargetPos[1] .. "," .. averagedTargetPos[2] .. "," .. averagedTargetPos[3] .. ",")
 		if IS_TRACKING then
 			local pos_now = TARGET_G_POS_AVE:getAveragedTable()
 			local pos_prev = TARGET_G_POS_AVE_P:getAveragedTable()
-			local gVel = { pos_now[1] - pos_prev[1], pos_now[2] - pos_prev[2], pos_now[3] - pos_prev[3] }
+			gVel = { pos_now[1] - pos_prev[1], pos_now[2] - pos_prev[2], pos_now[3] - pos_prev[3] }
 			for i = 1, 3 do
-				gPos[i] = gPos[i] + gVel[i] * (TIMELAG+6)
+				gPosRaw[i] = gPosRaw[i] + gVel[i] * (TIMELAG+6)
 			end
 		end
-		TARGET_G_POS_AVE:update(gPos)
-		debug.log("TST:->," ..TARGET_G_POS_AVE:getAveragedTable()[1] .. "," .. TARGET_G_POS_AVE:getAveragedTable()[2] .. "," .. TARGET_G_POS_AVE:getAveragedTable()[3] .. ",")
-		lPos = ATTITUDE_BASE:getFutureAttitude(10):rotateVectorWorldToLocal(TARGET_G_POS_AVE:getAveragedTable())
+		lPos = ATTITUDE_BASE:getFutureAttitude(10):rotateVectorWorldToLocal(averagedTargetPos)
 		PIVOT_H, PIVOT_V = getAngle(lPos)
-		TARGET_G_POS_P = TARGET_POS
 		TARGET_G_POS_AVE_P = TARGET_G_POS_AVE
 		if TARGET_G_POS_AVE_P:isStockFull() then
 			IS_TRACKING = true
@@ -58,6 +58,10 @@ function onTick()
 			PIVOT_V = clamp(PIVOT_V - INC, 1, -1)
 		end
 		IS_TRACKING = false
+	end
+	for i = 1, 3 do
+		output.setNumber(i, gPosRaw[i])
+		output.setNumber(i+3, gVel[i])
 	end
 	output.setNumber(31, PIVOT_V)
 	output.setNumber(32, PivotPID:update((PIVOT_H - input.getNumber(14) + 1.5) % 1 - 0.5, 0))
