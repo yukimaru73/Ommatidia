fd6d1bf5910db9be4036468a4dc66f00123f40e1
yukimaru73 <ekxmekxm@gmail.com>
2022-12-09 20:54:02 +0900

    divide eraced

--- FCS/3-RotatePivot.lua
+++ FCS/3-RotatePivot.lua
@@ -11,8 +11,8 @@ VERTICAL_SPEED = property.getNumber("Vertical Speed") / 100
 
 HORIZONTAL_CONTINUOUS = (MAX_HORIZONTAL_ANGLE + MIN_HORIZONTAL_ANGLE == 0) and (MAX_HORIZONTAL_ANGLE == 0.5)
 
-HORIZONTAL_UDC = UpDownCounter:new(HORIZONTAL_SPEED/100, MIN_HORIZONTAL_ANGLE*4, MAX_HORIZONTAL_ANGLE*4)
-VERTICAL_UDC = UpDownCounter:new(VERTICAL_SPEED/100, MIN_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE)
+HORIZONTAL_UDC = UpDownCounter:new(HORIZONTAL_SPEED / 100, MIN_HORIZONTAL_ANGLE * 4, MAX_HORIZONTAL_ANGLE * 4)
+VERTICAL_UDC = UpDownCounter:new(VERTICAL_SPEED / 100, MIN_VERTICAL_ANGLE, MAX_VERTICAL_ANGLE)
 
 IS_HORIZONTAL_PIVOT_VELOCITY = property.getBool("Horizontal Pivot")
 
@@ -22,6 +22,7 @@ PIVOT_H = 0
 PIVOT_H_OUT = 0
 PIVOT_V = 0
 PivotPID = PID:new(20, 0.005, 0.3, 0.08)
+DIV_PI2 = 1 / math.pi / 2
 
 function onTick()
 	---update self attitude
@@ -41,14 +42,14 @@ function onTick()
 		if HORIZONTAL_CONTINUOUS then
 			pidValue = PivotPID:update((clampedHorizontal - input.getNumber(13) + 1.5) % 1 - 0.5, 0)
 		else
-			pidValue = PivotPID:update(clampedHorizontal - input.getNumber(13), 0)
+			pidValue = PivotPID:update(clampedHorizontal - input.getNumber(13), cha0)
 		end
 		PIVOT_H_OUT = clamp(pidValue, -HORIZONTAL_SPEED, HORIZONTAL_SPEED)
 	else
-		PIVOT_H_OUT = HORIZONTAL_UDC:update(PIVOT_H*4)
+		PIVOT_H_OUT = HORIZONTAL_UDC:update(PIVOT_H * 4)
 	end
 	output.setNumber(1, PIVOT_H_OUT)
-	output.setNumber(2, VERTICAL_UDC:update(PIVOT_V*4))
+	output.setNumber(2, VERTICAL_UDC:update(PIVOT_V * 4))
 end
 
 function angleToPosition(azimuth, elevation)
@@ -62,7 +63,7 @@ function posiionToTurn(vector)
 	local azimuth, elevation
 	azimuth = math.atan(vector[3], vector[1])
 	elevation = math.atan(vector[2], math.sqrt(vector[1] ^ 2 + vector[3] ^ 2))
-	return azimuth / math.pi / 2, elevation / math.pi / 2
+	return azimuth * DIV_PI2, elevation  * DIV_PI2
 end
 
 function clamp(value, min, max)
